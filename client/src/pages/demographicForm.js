dbffe4fa2f0346578172fdb7a42a2431465f2ab0
root <root@Zacks-Zenbook.localdomain>
2023-03-04 11:22:22 -0700

    Added subject class, attempting to set class variables with input id from demographic survey

--- client/src/pages/demographicForm.js
+++ client/src/pages/demographicForm.js
@@ -1,7 +1,11 @@
 import {Button, Form, FormGroup, Label, Input, FormText, Table, Col} from 'reactstrap';
 import NavBar from "../components/NavBar";
+import subject from "../components/Subject"
 
 export default function DemographicForm(props){
+
+    const newSubject = new subject();
+    console.log("DEBUG: groupID=", newSubject.groupID);
 
     return(
         <div className={"App"}>
@@ -10,15 +14,15 @@ export default function DemographicForm(props){
                     <Label for="gender" sm={2}>Gender</Label>
                     <Col>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="female" />{' '}
                             female
                         </Label><br/>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="male" />{' '}
                             male
                         </Label><br/>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="other"/>{' '}
                             prefer not to say
                         </Label>
                     </Col>
@@ -27,7 +31,7 @@ export default function DemographicForm(props){
                 <FormGroup className={"formGroup"} row>
                     <Label for="gender">Age</Label>
                     <Col>
-                        <Input type="text" name="text1" />{' '}
+                        <Input type="text" name="text1" id="Age" />{' '}
                     </Col>
                 </FormGroup>
 
@@ -36,19 +40,19 @@ export default function DemographicForm(props){
                     <Col>
 
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="1-3"/>{' '}
                             1-3
                         </Label><br/>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="4-7"/>{' '}
                             4-7
                         </Label><br/>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="8-10"/>{' '}
                             8-10
                         </Label>
                         <Label check>
-                            <Input type="radio" name="radio1" />{' '}
+                            <Input type="radio" name="radio1" id="10+"/>{' '}
                             10+
                         </Label>
                     </Col>
