85a3aca08cf2d64e46d3c9b7f6ad807f137c7e15
Dan Butcher <danbobbutcher@gmail.com>
2023-03-24 11:24:01 -0600

    end point working

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 22222
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/addData', (request, response)=>{
-    console.log(request.body);
+    console.log("Got POST /addData" + JSON.stringify(request.body));
+    response.send({ success: "true"});
 })
 
 // Makes everything in the media folder available over http
