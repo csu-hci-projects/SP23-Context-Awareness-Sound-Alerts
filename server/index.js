d3ab175a664bf2622b67fc8289e1956ab347253f
Dan Butcher <danbobbutcher@gmail.com>
2023-03-24 10:57:37 -0600

    added mongodb dep

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 22222
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:<password>@cluster0.mglvzsy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
