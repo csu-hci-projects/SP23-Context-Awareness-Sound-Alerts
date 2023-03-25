dbc1b6788e3d6ac39f8d8265a97b63ce92c1ad2c
Dan <danbobbutcher@gmail.com>
2023-03-25 06:55:14 -0600

    added some comments

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -5,6 +5,8 @@ export default function Timer(props){
     const [startTime, setStartTime] = useState(new Date())
     const [elapsed, setElapsed] = useState(new Date(new Date() - startTime))
 
+    // Sets how often the display will refresh in milliseconds
+    // Essentially the resolution of the displayed timer
     setTimeout(()=>{
         setElapsed(new Date(new Date() - startTime))
     }, 100)
