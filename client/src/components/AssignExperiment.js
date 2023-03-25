527a31c4963ac3a2bd5803dea693a6edb21b68ba
zlamb94 <zlamb_94@hotmail.com>
2023-03-25 15:16:04 -0600

    Added experiment designs and ability to assign based on group number

---
+++ client/src/components/AssignExperiment.js
@@ -0,0 +1,23 @@
+import subject from "./Subject";
+import { experimentConfigA, experimentConfigB, experimentConfigC } from "./ExperimentDesign.js";
+
+
+
+export default function AssignExp(groupID) {
+
+    let group_char = subject.groupID.charAt(0)
+    var experiment_group;
+
+    switch (group_char) {
+        case 'A':
+            experiment_group = experimentConfigA;
+            break;
+        case 'B':
+            experiment_group = experimentConfigB;
+            break;
+        default:
+            experiment_group = experimentConfigC;
+    }
+
+    return experiment_group;
+}
