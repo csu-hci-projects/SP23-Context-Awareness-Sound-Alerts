e2dc2228841bd06d66172a6ce75592840e0425b1
zlamb94 <zlamb_94@hotmail.com>
2023-03-25 14:50:22 -0600

    added change to groupID for subjects

--- client/src/components/Subject.js
+++ client/src/components/Subject.js
@@ -75,14 +75,62 @@ export default class subject {
     }
 }
 
-function generate_groupID() {
-    var participant_num; //store number of particpants
+function generate_groupID(entries) {
     var unique_id_num; //store unique id number
+    var group_char;
+    var final_id;
 
-    //Set number of participants to generate the random id value
-    participant_num = 30;
+    switch (entries) {
+        case 0:
+            group_char = 'A';
+            break;
+        case 1:
+            group_char = 'B';
+            break;
+        case 2: 
+            group_char = 'C';
+            break;
+        case 3: 
+            group_char = 'A';
+            break;
+        case 4:
+            group_char = 'B'
+            break;
+        case 5:
+            group_char = 'C'
+            break;
+        case 6:
+            group_char = 'A'
+            break;
+        case 7:
+            group_char = 'B';
+            break;
+        case 8:
+            group_char = 'C';
+            break;
+        case 9: 
+            group_char = 'A';
+            break;
+        case 10: 
+            group_char = 'B';
+            break;
+        case 11:
+            group_char = 'C';
+            break;
+        case 12:
+            group_char = 'A';
+            break;
+        case 13:
+            group_char = 'B';
+            break;
+        default:
+            group_char = 'C'
+    }
 
     //randomly generate id for subject, can change to something with a standard distribution
-    unique_id_num = Math.floor(Math.random() * (participant_num + 10))
-    return unique_id_num;
+
+    unique_id_num = Math.floor(Math.random() * (10000))
+    unique_id_num = unique_id_num.toString();
+    final_id = group_char.concat("-", unique_id_num);
+    return final_id;
 }
