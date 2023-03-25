aad87a48cd8406392e09aa91214a31349e1ef7c1
zlamb94 <zlamb_94@hotmail.com>
2023-03-25 16:00:07 -0600

    testing1234

--- client/src/api/getExperimentCount.js
+++ client/src/api/getExperimentCount.js
@@ -5,5 +5,6 @@ export default async function getExperimentCount(){
         mode: "cors"
     }
     const response = await fetch(url, config);
+    console.log(response);
     return response.json();
 }
--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -8,6 +8,7 @@ export default function Data(props){
             <p>Age: {props.subject.age ? props.subject.age : null}</p>
             <p>Computer Use: {props.subject.computerUse ? props.subject.computerUse : null}</p>
             <p>Group ID: {props.subject.groupID ? props.subject.groupID : null}</p>
+            <p>Assigned Experiment: {props.subject.assignedExperiment ? props.subject.assignedExperiment : null}</p>
             <p>WPMs: {props.subject.wpm}</p>
             <p>Clicks: {props.subject.actions}</p>
         </div>
