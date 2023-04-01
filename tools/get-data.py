6059c460b38dbebb9428d5e50523c6c2ab8c522a
Dan <danbobbutcher@gmail.com>
2023-04-01 06:29:03 -0600

    starting to digest data

--- tools/get-data.py
+++ tools/get-data.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pandas as pd
 
 DATABASE = "test"
 COLLECTION = "test-collection"
@@ -14,9 +15,35 @@ client = MongoClient(key)
 db = client[DATABASE]
 collection = db[COLLECTION]
 
+# Data to use from the database
+columns = [
+    "database-id",
+    "groupID",
+    "city-start-time",
+    "city-end-time",
+    "city-alert1-time",
+    "city-alert2-time",
+    "city-alert3-time",
+    "city-wpm",
+    "beach-start-time",
+    "beach-end-time",
+    "beach-alert1-time",
+    "beach-alert2-time",
+    "beach-alert3-time",
+    "beach-wpm",
+    "airport-start-time",
+    "airport-end-time",
+    "airport-alert1-time",
+    "airport-alert2-time",
+    "airport-alert3-time",
+    "airport-wpm"
+]
+data = pd.DataFrame(columns=columns)
+
+
 item_count = 0
 for item in collection.find():
-    print(item)
+    pass
 
 print(item_count)
 
