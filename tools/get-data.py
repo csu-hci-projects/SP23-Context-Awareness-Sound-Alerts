c10f0a7c4a8574b062e5c93529f2b98569c64545
Dan <danbobbutcher@gmail.com>
2023-04-01 21:18:47 -0600

    csvs working

--- tools/get-data.py
+++ tools/get-data.py
@@ -4,36 +4,34 @@ import pandas as pd
 DATABASE = "test"
 COLLECTION = "test-collection"
 
-
-def error_check(value):
-    if value:
-        return value
-    else:
-        return "NA"
-
-
 # Master linking of data to source
 data_source = {
-    "database-id": lambda item: error_check(str(item['_id'])),
-    "groupID": lambda item: error_check(item['groupID']),
-    "city-start-time": lambda phases: error_check(phases['city']['experimentStartTime']),
-    "city-end-time": lambda phases: error_check(phases['city']['experimentEndTime']),
-    "city-alert1-time": lambda phases: error_check(phases['city']['alertTimes'][0]),
-    "city-alert2-time": lambda phases: error_check(phases['city']['alertTimes'][1]),
-    "city-alert3-time": lambda phases: error_check(phases['city']['alertTimes'][2]),
-    "city-wpm": lambda phases: error_check(phases['city']['wpm']),
-    "beach-start-time": lambda phases: error_check(phases['beach']['experimentStartTime']),
-    "beach-end-time": lambda phases: error_check(phases['beach']['experimentEndTime']),
-    "beach-alert1-time": lambda phases: error_check(phases['beach']['alertTimes'][0]),
-    "beach-alert2-time": lambda phases: error_check(phases['beach']['alertTimes'][1]),
-    "beach-alert3-time": lambda phases: error_check(phases['beach']['alertTimes'][2]),
-    "beach-wpm": lambda phases: error_check(phases['beach']['wpm']),
-    "airport-start-time": lambda phases: error_check(phases['airport']['experimentStartTime']),
-    "airport-end-time": lambda phases: error_check(phases['airport']['experimentEndTime']),
-    "airport-alert1-time": lambda phases: error_check(phases['airport']['alertTimes'][0]),
-    "airport-alert2-time": lambda phases: error_check(phases['airport']['alertTimes'][1]),
-    "airport-alert3-time": lambda phases: error_check(phases['airport']['alertTimes'][2]),
-    "airport-wpm": lambda phases: error_check(phases['airport']['wpm'])
+    "database-id": lambda item: str(item['_id']),
+    "groupID": lambda item: item['groupID'],
+    "gender": lambda item: item['gender'],
+    "age": lambda item: item['age'],
+    "computerUse": lambda item: item['computerUse'],
+    "brand": lambda item: item['brand'],
+    "silent": lambda item: item['silent'],
+    "defaultNotification": lambda item: item['defaultNotification'],
+    "city-start-time": lambda phases: phases['city']['experimentStartTime'],
+    "city-end-time": lambda phases: phases['city']['experimentEndTime'],
+    "city-alert1-time": lambda phases: phases['city']['alertTimes'][0],
+    "city-alert2-time": lambda phases: phases['city']['alertTimes'][1],
+    "city-alert3-time": lambda phases: phases['city']['alertTimes'][2],
+    "city-wpm": lambda phases: phases['city']['wpm'],
+    "beach-start-time": lambda phases: phases['beach']['experimentStartTime'],
+    "beach-end-time": lambda phases: phases['beach']['experimentEndTime'],
+    "beach-alert1-time": lambda phases: phases['beach']['alertTimes'][0],
+    "beach-alert2-time": lambda phases: phases['beach']['alertTimes'][1],
+    "beach-alert3-time": lambda phases: phases['beach']['alertTimes'][2],
+    "beach-wpm": lambda phases: phases['beach']['wpm'],
+    "airport-start-time": lambda phases: phases['airport']['experimentStartTime'],
+    "airport-end-time": lambda phases: phases['airport']['experimentEndTime'],
+    "airport-alert1-time": lambda phases: phases['airport']['alertTimes'][0],
+    "airport-alert2-time": lambda phases: phases['airport']['alertTimes'][1],
+    "airport-alert3-time": lambda phases: phases['airport']['alertTimes'][2],
+    "airport-wpm": lambda phases: phases['airport']['wpm']
 }
 
 
@@ -67,7 +65,7 @@ def get_data(item, phases):
     data_types = data_source.keys()
     for d in data_types:
         try:
-            if d in ['database-id', 'groupID']:
+            if d in ['database-id', 'groupID', 'gender', 'age', 'computerUse', 'brand', 'silent', 'defaultNotification']:
                 result.append(data_source[d](item))
             else:
                 result.append(data_source[d](phases))
@@ -80,12 +78,13 @@ def get_data(item, phases):
 
 
 columns = list(data_source.keys())
-data = pd.DataFrame(columns=columns)
 
+data = list()
 for item in collection.find():
     phases = find_order(item['assignedExperiment'], item['phaseData'])
     row_data = get_data(item, phases)
-    row = pd.DataFrame(row_data, columns=columns)
-    pd.concat([data, row])
+    data.append(row_data)
 
-data.to_csv("test_csv.csv")
+final_data = pd.DataFrame(data, columns=columns)
+
+final_data.to_csv("test_csv.csv")
