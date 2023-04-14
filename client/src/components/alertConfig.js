1bf6c1e203d2e0c98815bcb77fe4ef594865167a
Dan Butcher <danbobbutcher@gmail.com>
2023-04-14 13:01:00 -0600

    fixed shortened timings

--- client/src/components/alertConfig.js
+++ client/src/components/alertConfig.js
@@ -14,10 +14,7 @@ const alertSound3 = {
 export const alertConfig = {
     sounds: [alertSound1, alertSound2, alertSound3],
     // THESE ARE ALL SHORT TIMES NOW FOR EASY TESTING
-    // padding_ms: 15 * 1000, // Minimum space between alerts
-    // potentialLength_ms: 45 * 1000, // The amount of time an alert could possibly be played after padding
-    // frontPadding_ms: 15 * 1000, // Minimum space before first alert
-    padding_ms: 5 * 1000, // Minimum space between alerts
-    potentialLength_ms: 15 * 1000, // The amount of time an alert could possibly be played after padding
-    frontPadding_ms: 1 * 1000, // Minimum space before first alert
+    padding_ms: 15 * 1000, // Minimum space between alerts
+    potentialLength_ms: 45 * 1000, // The amount of time an alert could possibly be played after padding
+    frontPadding_ms: 15 * 1000, // Minimum space before first alert
 }
